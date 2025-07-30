06f8011cd7a32ae21f08e89786efc7109d7f07de
XeVolo <xevolo12@gmail.com>
2025-07-30 16:48:19 +0200

    Swagger

--- RecruitmentTaskApp/API/Program.cs
+++ RecruitmentTaskApp/API/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient<ICatFactService, CatFactService>();
 builder.Services.AddScoped<ILoggingService, LoggingService>();
 
@@ -15,6 +17,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
